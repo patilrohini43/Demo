206208e859d1a6c75ed85bfbaf340d24a381747e
patilrohini43 <45816823+patilrohini43@users.noreply.github.com>
2019-12-06 15:23:52 +0530

    modify message hello world

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public static void main(String[] args)
     while (true)
  {
      try{
-       System.out.println(" Hello");
+       System.out.println(" Hello World");
         Thread.sleep(6000);
      }catch(Exception ex){ }
   } 
